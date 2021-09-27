5de92c2309728c864a279f9056305ef28108ffff
TS93 <tschram93@yahoo.com>
2021-09-27 17:34:51 -0400

    psuedocode TODO steps of the project.

---
+++ script.js
@@ -0,0 +1,18 @@
+//  Establish variables for the NODE modules
+
+// Incorporate Inquirer with syntax to ask questions to generate README using array.
+// Prompt for question answer confirmation(Validate)
+
+// Format of syntax
+// type:
+// message:
+// name:
+
+
+// Template 
+
+// Function to create README using "fs"
+
+// Function for createFile
+
+// Call Function for app initialization

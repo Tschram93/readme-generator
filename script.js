706ec8a037fa4c70f7342d800aaa226629f5e359
TS93 <tschram93@yahoo.com>
2021-09-27 18:10:33 -0400

    wrote questions for inquirer

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const fs = require('fs');
 // const;
 
 // TODO: Incorporate Inquirer with syntax to ask questions to generate README using array.
+
+// Format of syntax
 // type: 
 // message: 
+// choices: (if any.. else leave out)
+// default: (if any.. else leave out)
 // name: 
 
 inquirer.prompt(
@@ -18,12 +22,53 @@ inquirer.prompt(
             type: 'input',
             message: 'What is your Github username?',
             name: 'github',
-
         },
-
+        {
+            type: 'input',
+            message: "What is your email address?",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "Give a description of your project.",
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: "What is the link to the page or demo?",
+            name: 'link',
+        },
+        {
+            type: 'list',
+            message: "What license does your project use?",
+            name: 'license',
+            choices: [
+                'MIT',
+                'Apache 2.0',
+                'BSD 3',
+                'GPL 3.0',
+                'none'
+            ]
+        },
+        {
+            type: 'input',
+            message: 'Explain with instructions on how to use this application.',
+            name: 'instructions',
+        },
+        {
+            type: 'default',
+            message: 'What is the command to install the required dependencies?',
+            default: 'npm i',
+            name: 'dependencies',
+        },
+        {
+            type: 'input',
+            message: 'What needs to be done in order to run tests?',
+            default: 'npm test',
+            name: 'tests',
+        },
         // TODO: Prompt for question answer confirmation(Validate)
 
-        // Format of syntax
     ]
 )
 // Template 
